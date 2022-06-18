c908852c8d35fea0ece8caa9f4ccc756b14712b3
Angelo Kim Hui Lim <angelokimhui@gmail.com>
2022-06-18 21:40:53 +0800

    Add wallpapers and migrate to BSPWM

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
 # Terminate already running bar instances
-# If all your bars have ipc enabled, you can use 
-polybar-msg cmd quit
-# Otherwise you can use the nuclear option:
-# killall -q polybar
+killall -q polybar
 
-# Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+# Wait until the processes have been shut down
+while pgrep -x polybar >/dev/null; do sleep 1; done
+
+# Launch statusbar
 polybar statusbar & disown
